bd81caa7fddf04e4250fd3afdffb1fa981afd122
Luana Beatriz Souza Venâncio <luanavenancio73643@gmail.com>
2025-02-22 19:18:49 -0300

    feat: estrutura arquivo

---
+++ src/main/java/org/example/Arquivo/Arquivo.java
@@ -0,0 +1,158 @@
+package org.example.Arquivo;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class Arquivo {
+
+    private String nomearquivo;
+    private RandomAccessFile arquivo;
+
+    private int comparacoes = 0;
+    private int movimentacoes = 0;
+
+    public Arquivo(String nomearquivo) {
+        try {
+            arquivo = new RandomAccessFile(nomearquivo, "rw");
+        } catch (IOException e) {
+        }
+    }
+
+    public int getComparacoes() {
+        return comparacoes;
+    }
+
+    public void setComparacoes(int comparacoes) {
+        this.comparacoes = comparacoes;
+    }
+
+    public int getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    public void setMovimentacoes(int movimentacoes) {
+        this.movimentacoes = movimentacoes;
+    }
+
+    public RandomAccessFile getFile() {
+        return arquivo;
+    }
+
+    public void truncate(long pos) {
+        try {
+            arquivo.setLength(pos * Registro.length());
+        } catch (IOException exc) {
+        }
+    }
+
+    public boolean eof() {
+        boolean retorno = false;
+        try {
+            if (arquivo.getFilePointer() == arquivo.length())
+                retorno = true;
+        } catch (IOException e) {
+        }
+        return (retorno);
+    }
+
+    //insere um Registro no final do arquivo, passado por par�metro
+    public void inserirRegNoFinal(Registro reg) {
+        seekArq(filesize());//ultimo byte
+        reg.gravaNoArq(arquivo);
+    }
+
+    public int filesize() {
+        try {
+            return (int) arquivo.length() / Registro.length();
+        } catch (IOException e) {
+            return 0;
+        }
+    }
+
+    public void exibirArq() {
+        int i;
+        Registro aux = new Registro();
+        seekArq(0);
+        i = 0;
+        while (!this.eof()) {
+            //System.out.println("Posicao " + i);
+            aux.leDoArq(arquivo);
+            aux.exibirReg();
+            i++;
+        }
+    }
+
+    public void exibirUmRegistro(int pos) {
+        Registro aux = new Registro();
+        seekArq(pos);
+        //System.out.println("Posicao " + pos);
+        aux.leDoArq(arquivo);
+        aux.exibirReg();
+    }
+
+    public void seekArq(int pos) {
+        try {
+            arquivo.seek(pos * Registro.length());
+        } catch (IOException e) {
+        }
+    }
+
+    public void geraArquivoOrdenado(int tam) {
+        Registro reg;
+        for (int i = 1; i <= tam; i++) {
+            reg = new Registro(i);
+            reg.gravaNoArq(arquivo);
+        }
+    }
+
+    public void geraArquivoDesordenado(int tam) {
+        Registro reg;
+        Random random = new Random();
+        for (int i = 0; i < tam; i++) {
+            reg = new Registro(random.nextInt(1000) + 1);
+            reg.gravaNoArq(arquivo);
+        }
+    }
+
+    public void geraArquivoReverso(int tam) {
+        Registro reg;
+        for (int i = tam; i > 0; i--) {
+            reg = new Registro(i);
+            reg.gravaNoArq(arquivo);
+        }
+    }
+
+
+    public void copiaArq(RandomAccessFile arquivoOrigem) {
+        try {
+            Registro reg = new Registro();
+            int i = 0, tam = (int) arquivoOrigem.length() / Registro.length();
+            this.arquivo = new RandomAccessFile("temp.dat", "rw");
+            truncate(0);
+            arquivoOrigem.seek(0);
+            while (i < tam) {
+                reg.leDoArq(arquivoOrigem);
+                reg.gravaNoArq(arquivo);
+                i++;
+            }
+        } catch (IOException e) {
+
+        }
+    }
+//.............................................................................
+    /*
+
+    insira aqui os m�todos de Ordena��o;
+
+    */
+}
---
+++ src/main/java/org/example/Arquivo/Registro.java
@@ -0,0 +1,65 @@
+package org.example.Arquivo;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+class Registro
+{
+    public final int tf=1022;
+    private int numero; //4 bytes
+    private char lixo[] = new char[tf]; //2044 bytes
+    public Registro(int numero)
+    {
+        this.numero=numero;
+        for (int i=0 ; i<tf ; i++)
+            lixo[i]='X';
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public char[] getLixo() {
+        return lixo;
+    }
+
+    public void setLixo(char[] lixo) {
+        this.lixo = lixo;
+    }
+
+    public Registro() {}
+    public void gravaNoArq(RandomAccessFile arquivo)
+    {
+        try
+        {
+            arquivo.writeInt(numero);
+            for(int i=0 ; i<tf ; i++)
+                arquivo.writeChar(lixo[i]);
+        }catch(IOException e){}
+    }
+    public void leDoArq(RandomAccessFile arquivo)
+    {
+        try
+        {
+            numero = arquivo.readInt();
+            for(int i=0 ; i<tf ; i++)
+                lixo[i]=arquivo.readChar();
+        }catch(IOException e){}
+    }
+
+    public void exibirReg() {
+        System.out.println("Código: " + numero);
+    }
+    static int length()
+    {
+        //int numero; 4 bytes
+        //char lixo[] = new char[tf]; 2044 bytes
+        //--------------------------------------
+        // 2048 bytes
+        return(2048);
+    }
+}
